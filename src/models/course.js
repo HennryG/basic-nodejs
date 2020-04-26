0122859b375b7f9cdd42eee817bd78d8fea9940f
Hennry García - Personal <hennry.gb@gmail.com>
2020-04-26 14:54:43 -0500

    Activity 1

---
+++ src/models/course.js
@@ -0,0 +1,10 @@
+class Course {
+  constructor(id, name, duration, cost) {
+    this.id = id;
+    this.name = name;
+    this.duration = duration;
+    this.cost = cost;
+  }
+}
+
+module.exports = Course;
